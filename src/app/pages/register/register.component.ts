a6bd06d4412ea7992b90b191c25fdf4f18a15d59
Mechztcc <dojathan@gmail.com>
2021-01-25 10:07:53 -0300

    Add form control to register aoge

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public toast: boolean = false;
+  public form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+
+   }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      username: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(55)
+      ])],
+      email: ['', Validators.compose([
+        Validators.required,
+        Validators.email
+      ])],
+      password: ['', Validators.required],
+      date: ['', Validators.required],
+      zip: ['', Validators.required],
+      address: ['', Validators.compose([
+        Validators.minLength(10),
+        Validators.maxLength(55),
+        Validators.required
+      ])]
+    })
   }
+
+  makeToast() {
+    this.toast = true;
+  }
+
 
 }
